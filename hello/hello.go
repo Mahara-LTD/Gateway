52ff2253a3d3bf9f22b8065db253f5ee99e03917
Ramy Mawal <rmawal98@gmail.com>
2024-02-21 21:00:07 +0200

    Add CODEOWNERS

--- hello/hello.go
+++ hello/hello.go
@@ -7,6 +7,23 @@ import (
 )
 
 func SayHelloMadam() {
-	message := greetings.Hello("Ma'am")
+	message, err := greetings.Hello("Ma'am")
+
+	if err == nil {
+		fmt.Println(message)
+	}
+}
+
+func SayHello(name string) {
+
+	message, err := greetings.Hello(name)
+
+	if err != nil {
+		fmt.Printf("Error occured when using name: %s \n", name)
+
+		fmt.Println(err)
+	}
+
 	fmt.Println(message)
+	return
 }
--- main.go
+++ main.go
@@ -9,5 +9,5 @@ import (
 func main() {
 	fmt.Printf("Running now...\n")
 
-	hello.SayHelloMadam()
+	hello.SayHello("Maine")
 }
